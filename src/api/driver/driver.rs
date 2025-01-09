064192416256d6d95bc527e7e417a69ae3f87f3c
Кирилл Донцов <KDontsov@integrity-solutions.ru>
2025-01-09 17:10:47 +0300

    chrome

--- src/api/driver/driver.rs
+++ src/api/driver/driver.rs
@@ -4,35 +4,35 @@ pub trait Driver {}
 
 impl dyn Driver {
 	pub async fn get_driver() -> Result<WebDriver, WebDriverError> {
-		// let mut caps = DesiredCapabilities::chrome();
+		let mut caps = DesiredCapabilities::chrome();
 
 		// без загрузки изображений
-		// caps.insert_browser_option(
-		// 	"prefs",
-		// 	serde_json::json!({
-		// 		"profile.default_content_settings": {
-		// 			"images": 2
-		// 		},
-		// 		"profile.managed_default_content_settings": {
-		// 			"images": 2
-		// 		}
-		// 	}),
-		// )?;
-		// let _ = caps.set_headless();
-		// let _ = caps.set_page_load_strategy(PageLoadStrategy::Eager)?;
-		// let _ = caps.add_arg("enable-automation");
-		// let _ = caps.add_arg("--no-sandbox");
-		// let _ = caps.add_arg("--disable-extensions");
-		// let _ = caps.add_arg("--dns-prefetch-disable");
-		// let _ = caps.add_arg("--disable-gpu");
-		// let _ = caps.add_arg("enable-features=NetworkServiceInProcess");
+		caps.insert_browser_option(
+			"prefs",
+			serde_json::json!({
+				"profile.default_content_settings": {
+					"images": 2
+				},
+				"profile.managed_default_content_settings": {
+					"images": 2
+				}
+			}),
+		)?;
+		let _ = caps.set_headless();
+		let _ = caps.set_page_load_strategy(PageLoadStrategy::Eager)?;
+		let _ = caps.add_arg("enable-automation");
+		let _ = caps.add_arg("--no-sandbox");
+		let _ = caps.add_arg("--disable-extensions");
+		let _ = caps.add_arg("--dns-prefetch-disable");
+		let _ = caps.add_arg("--disable-gpu");
+		let _ = caps.add_arg("enable-features=NetworkServiceInProcess");
 
-		// let driver = WebDriver::new("http://localhost:9515", caps).await;
+		let driver = WebDriver::new("http://localhost:9515", caps).await;
 
 		// firefox
-		let mut caps = DesiredCapabilities::firefox();
-		let _ = caps.set_page_load_strategy(PageLoadStrategy::None)?;
-		let driver = WebDriver::new("http://localhost:4444", caps).await;
+		// let mut caps = DesiredCapabilities::firefox();
+		// let _ = caps.set_page_load_strategy(PageLoadStrategy::None)?;
+		// let driver = WebDriver::new("http://localhost:4444", caps).await;
 		driver
 	}
 }
