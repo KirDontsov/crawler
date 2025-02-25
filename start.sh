c63b60ecb18c519d898b785c361518fddfc6dfb3
Кирилл Донцов <KDontsov@integrity-solutions.ru>
2025-02-25 16:24:53 +0300

    feat: add env.example

--- start.sh
+++ start.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 #$ cat start.sh
 
-echo "├➔ Проверяем от какого пользователя запускаем скрипт | $(whoami)"
-
-echo "├➔ Проверяем директорию в которой находимся          | $(pwd)"
-
-echo "├➔ Проверяем обновления                              | $(git pull)"
-
-echo "├➔ Компилируем crawler                               | $(cargo build -r)"
+echo "├➔ Проверяем от какого пользователя запускаем скрипт   | $(whoami)"
+echo "├➔ Проверяем директорию в которой находимся            | $(pwd)"
+echo "├➔ Проверяем обновления                                | $(git pull)"
+echo "├➔ Компилируем crawler                                 |"
+$(cargo build -r)
 
 # запускаем драйвер браузера
 
 chromedriver_drv=$(pgrep chromedriver)
 if [ -z $chromedriver_drv ]
 then
+echo "├➔ Запускаем драйвер для работы браузера               |"
 ./chromedriver --port=9515 --disable-gpu --dns-prefetch-disable --disable-extensions --no-sandbox enable-automation &
-echo "├➔ Запускаем драйвер для работы браузера             |"
+sleep 5
+echo "├➔ Драйвер запущен                                     | $(pgrep chromedriver)"
 else
-echo "├➔ Драйвер для работы браузер уже запущен            | $(pgrep chromedriver)"
+echo "├➔ Драйвер для работы браузера уже запущен             | $(pgrep chromedriver)"
 fi
-echo "└➔ Запускаем парсер                                  | crawler"
+echo "└➔ Запускаем СКАНЕР! (НЕ стартует? Повторите попытку!) | crawler"
 target/release/crawler
