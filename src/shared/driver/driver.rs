64bc6f4a78d6ebd51d2d28f091d1c47818436bb9
Кирилл Донцов <KDontsov@integrity-solutions.ru>
2025-02-25 17:00:32 +0300

    feat: add firefox config

--- src/shared/driver/driver.rs
+++ src/shared/driver/driver.rs
@@ -5,9 +5,9 @@ pub trait Driver {}
 
 impl dyn Driver {
 	pub async fn get_driver() -> Result<WebDriver, WebDriverError> {
-		let headless = env::var("HEADLESS_CHROME").expect("HEADLESS_CHROME not set");
+		// let headless = env::var("HEADLESS_CHROME").expect("HEADLESS_CHROME not set");
 
-		let mut caps = DesiredCapabilities::chrome();
+		// let mut caps = DesiredCapabilities::chrome();
 
 		// без загрузки изображений
 		// caps.insert_browser_option(
@@ -23,29 +23,29 @@ impl dyn Driver {
 		// )?;
 		//
 
-		if headless.parse().unwrap() {
-			let _ = caps.set_headless();
-		}
+		// if headless.parse().unwrap() {
+		// 	let _ = caps.set_headless();
+		// }
 
-		let _ = caps.set_page_load_strategy(PageLoadStrategy::Eager)?;
-		let _ = caps.add_arg("enable-automation");
-		let _ = caps.add_arg("--no-sandbox");
-		let _ = caps.add_arg("--disable-extensions");
-		let _ = caps.add_arg("--dns-prefetch-disable");
-		let _ = caps.add_arg("--disable-gpu");
-		let _ = caps.add_arg("--disable-background-mode");
-		let _ = caps.add_arg("--disable-sync");
-		let _ = caps.add_arg("--disable-translate");
-		let _ = caps.add_arg("--disable-plugins");
-		let _ = caps.add_arg("--purge-memory-button");
-		let _ = caps.add_arg("enable-features=NetworkServiceInProcess");
+		// let _ = caps.set_page_load_strategy(PageLoadStrategy::Eager)?;
+		// let _ = caps.add_arg("enable-automation");
+		// let _ = caps.add_arg("--no-sandbox");
+		// let _ = caps.add_arg("--disable-extensions");
+		// let _ = caps.add_arg("--dns-prefetch-disable");
+		// let _ = caps.add_arg("--disable-gpu");
+		// let _ = caps.add_arg("--disable-background-mode");
+		// let _ = caps.add_arg("--disable-sync");
+		// let _ = caps.add_arg("--disable-translate");
+		// let _ = caps.add_arg("--disable-plugins");
+		// let _ = caps.add_arg("--purge-memory-button");
+		// let _ = caps.add_arg("enable-features=NetworkServiceInProcess");
 
-		let driver = WebDriver::new("http://localhost:9515", caps).await;
+		// let driver = WebDriver::new("http://localhost:9515", caps).await;
 
 		// firefox
-		// let mut caps = DesiredCapabilities::firefox();
-		// let _ = caps.set_page_load_strategy(PageLoadStrategy::None)?;
-		// let driver = WebDriver::new("http://localhost:4444", caps).await;
+		let mut caps = DesiredCapabilities::firefox();
+		let _ = caps.set_page_load_strategy(PageLoadStrategy::None)?;
+		let driver = WebDriver::new("http://localhost:4444", caps).await;
 		driver
 	}
 }
